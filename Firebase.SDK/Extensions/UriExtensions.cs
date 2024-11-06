8c012f360b81fa4a435a74740af87dfa8468b40a
Victor Blomqvist <129163377+viblo-majority@users.noreply.github.com>
2024-11-06 13:56:13 +0100

    Fix Uri append relative uris (#8)
    * Fix Uri append relative uris
    
    * Cleanup

---
+++ Firebase.SDK/Extensions/UriExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Firebase.SDK.Extensions;
+
+public static class UriExtensions
+{
+    public static Uri Append(this Uri baseUri, string relativeUrl)
+    {
+        var baseUrl = baseUri.AbsoluteUri;
+
+        if (string.IsNullOrWhiteSpace(relativeUrl))
+            return new Uri(baseUrl);
+
+        baseUrl = baseUrl.TrimEnd('/');
+        relativeUrl = relativeUrl.TrimStart('/');
+
+        return new Uri($"{baseUrl}/{relativeUrl}");
+    }
+}
--- Firebase.SDK/HttpClients/FirebaseHttpClient.cs
+++ Firebase.SDK/HttpClients/FirebaseHttpClient.cs
@@ -1,61 +1,53 @@
-﻿namespace Firebase.SDK.HttpClients
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Firebase.SDK.Configuration;
+using Firebase.SDK.Extensions;
+using Firebase.SDK.Firebase.Auth.ServiceAccounts;
+
+namespace Firebase.SDK.HttpClients;
+
+internal abstract class FirebaseHttpClient : IFirebaseHttpClient
 {
-    #region Namespace Imports
+    protected readonly Uri Authority;
+    protected readonly IHttpClientProxy Client;
+    protected readonly FirebaseSDKConfiguration Configuration;
+    protected readonly IServiceAccountCredentials Credentials;
 
-    using System;
-    using System.Net.Http;
-    using System.Net.Http.Headers;
-    using System.Threading.Tasks;
-
-    using Configuration;
-    using Extensions;
-    using Firebase.Auth.ServiceAccounts;
-
-    #endregion
-
-
-    internal abstract class FirebaseHttpClient : IFirebaseHttpClient
+    protected FirebaseHttpClient(IServiceAccountCredentials credentials, FirebaseSDKConfiguration configuration,
+        Uri authority = null)
     {
-        protected readonly Uri Authority;
-        protected readonly IHttpClientProxy Client;
-        protected readonly FirebaseSDKConfiguration Configuration;
-        protected readonly IServiceAccountCredentials Credentials;
+        Credentials = credentials;
+        Configuration = configuration;
+        Authority = authority;
+        Client = Configuration.HttpClientProxy(configuration);
+    }
 
-        protected FirebaseHttpClient(IServiceAccountCredentials credentials, FirebaseSDKConfiguration configuration, Uri authority = null)
-        {
-            Credentials = credentials;
-            Configuration = configuration;
-            Authority = authority;
-            Client = Configuration.HttpClientProxy(configuration);
-        }
+    public Uri GetAuthority()
+    {
+        return Authority;
+    }
 
-        public Uri GetAuthority() => Authority;
+    protected void AddAuthHeaders(HttpRequestMessage request)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
 
-        protected void AddAuthHeaders(HttpRequestMessage request)
-        {
-            if (request == null)
-            {
-                throw new ArgumentNullException(nameof(request));
-            }
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Configuration.AccessToken?.AccessToken);
+    }
 
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Configuration.AccessToken?.AccessToken);
-        }
+    protected Uri GetFullAbsoluteUrl(Uri uri)
+    {
+        if (Authority == null && !uri.IsAbsoluteUri)
+            throw new ArgumentOutOfRangeException(nameof(uri), "If Authority is missing uri should be absolute");
 
-        protected Uri GetFullAbsoluteUrl(Uri uri)
-        {
-            if (Authority == null && !uri.IsAbsoluteUri)
-            {
-                throw new ArgumentOutOfRangeException(nameof(uri), "If Authority is missing uri should be absolute");
-            }
+        if (uri.IsAbsoluteUri) return uri;
 
-            if (uri.IsAbsoluteUri)
-            {
-                return uri;
-            }
+        return Authority == null ? uri : Authority.Append(uri.ToString());
+    }
 
-            return Authority == null ? uri : new Uri(Authority, uri);
-        }
-
-        protected Task<string> SendAsync(Func<HttpRequestMessage> requestMessage) => Client.SendAsync(requestMessage);
+    protected Task<string> SendAsync(Func<HttpRequestMessage> requestMessage)
+    {
+        return Client.SendAsync(requestMessage);
     }
 }
